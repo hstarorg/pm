9f0d3bb9af7832ad7aaa4221c65780e3463b407d
Hstarorg <hm910705@163.com>
2018-01-07 23:50:34 +0800

    Add postgreSql tests

--- backend-api/__tests__/PostgreSQLClient.test.js
+++ backend-api/__tests__/PostgreSQLClient.test.js
@@ -3,24 +3,84 @@ const PostgreSQLClient = require('../src/db-providers/PostgreSQLClient');
 
 describe('PostgreSQLClient', () => {
   let client = new PostgreSQLClient(null);
-  beforeEach(async () => {
+
+  // Create the database
+  beforeAll(async done => {
     const pool = new Pool({
       host: '192.168.1.200',
       user: 'root',
       password: 'humin',
-      database: 'pm_db',
+      database: 'postgres',
       max: 20,
       idleTimeoutMillis: 30000,
       connectionTimeoutMillis: 2000
     });
     client = new PostgreSQLClient(pool);
+    const createTableSql = `
+CREATE TABLE "pm_db"."user" (
+  "id" serial PRIMARY KEY,
+  "name" varchar(50) NOT NULL,
+  "age" integer NOT NULL DEFAULT 0
+);
+    `;
+    await client.executeNonQuery(createTableSql);
+    done();
   });
 
-  it('should print types', () => {
-    console.log(types);
+  // Clear the database
+  afterAll(async done => {
+    const deleteTableSql = `DROP TABLE "pm_db"."user"`;
+    await client.executeNonQuery(deleteTableSql);
+    done();
   });
 
-  it('create table', async () => {
-    client.executeQuery('CREATE TABLE user(id int, name varchar(50), age int)');
+  it('query data', async () => {
+    const sql = `
+SELECT COUNT(0)::integer FROM "pm_db"."user"
+    `;
+    const data = await client.executeScalar(sql);
+    expect(data.count).toEqual(0);
   });
+
+  it('insert data', async () => {
+    const sql = `
+INSERT INTO "pm_db"."user"(name, age)
+VALUES(@name, @age) RETURNING id;
+    `;
+    const data = await client.executeScalar(sql, { name: 'humin', age: 15 });
+    expect(data.id).toEqual(1);
+  });
+
+  it('update data', async () => {
+    let sql = `
+UPDATE "pm_db"."user"
+SET name = 'jay';
+    `;
+    const count = await client.executeNonQuery(sql);
+    expect(count).toEqual(1);
+    sql = `
+SELECT * FROM "pm_db"."user"
+    `;
+    const rows = await client.executeQuery(sql);
+    expect(rows.length).toEqual(1);
+    expect(rows[0].name).toEqual('jay');
+  });
+
+  it('delete data', async () => {
+    let sql = `
+DELETE FROM "pm_db"."user"
+WHERE name = @name;
+    `;
+    const count = await client.executeNonQuery(sql, { name: 'jay' });
+    expect(count).toEqual(1);
+    sql = `
+SELECT * FROM "pm_db"."user"
+    `;
+    const rows = await client.executeQuery(sql);
+    expect(rows.length).toEqual(0);
+  });
+
+  it('tran commit');
+
+  it('tran rollback');
 });
--- backend-api/src/db-providers/PostgreSQLClient.js
+++ backend-api/src/db-providers/PostgreSQLClient.js
@@ -6,86 +6,58 @@ class PostgreSQLClient {
   /**
    * Begin database transaction
    */
-  beginTransaction() {
-    return this._getConnection().then(conn => {
-      return new Promise((resolve, reject) => {
-        conn.beginTransaction(err => {
-          if (err) {
-            return reject(err);
-          }
-          conn.inTransaction = true;
-          resolve(conn);
-        });
-      });
-    });
+  async beginTransaction() {
+    const client = await this._getClient();
+    return client.query('BEGIN');
   }
 
   /**
    * Commit transaction
-   * @param {any} conn
+   * @param {any} client
    */
-  commitTransaction(conn) {
-    return new Promise((resolve, reject) => {
-      conn.commit(err => {
-        if (err) {
-          conn.rollback(() => {
-            this._releaseConnection(conn, true);
-            reject(err);
-          });
-        } else {
-          this._releaseConnection(conn, true);
-          resolve();
-        }
-      });
-    });
+  commitTransaction(client) {
+    return client.query('COMMIT');
   }
 
   /**
    * Rollback transaction
-   * @param {any} conn
+   * @param {any} client
    */
-  rollbackTransaction(conn) {
-    return new Promise((resolve, reject) => {
-      conn.rollback(() => {
-        this._releaseConnection(conn, true);
-        resolve();
-      });
-    });
+  rollbackTransaction(client) {
+    return client.query('ROLLBACK');
   }
 
   /**
    * Get a database connection
-   * ？？为什么要把取得连接的私有方法暴露出去呢
-   * 答：方便执行Mysql这个lib提供的原生方法
    */
   getConnection() {
-    return this._getConnection();
+    return this._getClient();
   }
 
   /**
    * Execute sql, return query result(array)
    * @param {string} sqlString
    * @param {object | array} values
-   * @param {any} conn
+   * @param {any} client
    */
-  // 返回查询返回的结果数组
-  executeQuery(sqlString, values, conn = null) {
-    return this._execute(sqlString, values, conn);
+  executeQuery(sqlString, values, client = null) {
+    return this._execute(sqlString, values, client).then(res => {
+      return res.rows;
+    });
   }
 
   /**
    * Execute sql, return single result(the first one)
    * @param {string} sqlString
    * @param {object | array} values
-   * @param {any} conn
+   * @param {any} client
    */
-  // 返回查询到的第一个
-  executeScalar(sqlString, values, conn = null) {
-    return this._execute(sqlString, values, conn).then(results => {
-      if (results.length === 0) {
+  executeScalar(sqlString, values, client = null) {
+    return this._execute(sqlString, values, client).then(res => {
+      if (res.rows.length === 0) {
         return null;
       }
-      return results[0];
+      return res.rows[0];
     });
   }
 
@@ -93,49 +65,27 @@ class PostgreSQLClient {
    * Execute sql, return affected rows
    * @param {string} sqlString
    * @param {object | array} values
-   * @param {any} conn
+   * @param {any} client
    */
-  // 返回受影响的行数
-  executeNonQuery(sqlString, values, conn = null) {
-    return this._execute(sqlString, values, conn).then(results => {
-      return results.affectedRows;
-    });
-  }
-
-  /**
-   * Execute sql, return insertId(for auto id)
-   * @param {string} sqlString
-   * @param {object | array} values
-   * @param {any} conn
-   */
-  executeInsert(sqlString, values, conn = null) {
-    return this._execute(sqlString, values, conn).then(results => {
-      return results.insertId;
+  executeNonQuery(sqlString, values, client = null) {
+    return this._execute(sqlString, values, client).then(res => {
+      return res.rowCount;
     });
   }
 
   /**
    * private function, get db connection
-   * 从连接池中取得连接并打开mysql数据库
    */
-  _getConnection() {
-    return new Promise((resolve, reject) => {
-      this.pool.getConnection((err, conn) => {
-        if (err) {
-          return reject(err);
-        }
-        resolve(conn);
-      });
-    });
+  _getClient() {
+    return this.pool.connect();
   }
 
   /**
    * Release connection
-   * 释放连接
    * @param {any} conn
    * @param {boolean} closeTran
    */
-  _releaseConnection(conn, closeTran = false) {
+  _releaseClient(conn, closeTran = false) {
     if (closeTran) {
       conn.inTransaction = false;
     }
@@ -146,37 +96,41 @@ class PostgreSQLClient {
    * Execute sql, return resuluts;
    * @param {string} sqlString
    * @param {object|array} values
-   * @param {any} conn
+   * @param {any} client
    */
-  _execute(sqlString, values, conn) {
-    // 通过格式化参数，将占位符替换为具体数值，变成一条可执行的SQL语句
-    let { sql, params } = this._processSqlAndParameter(sqlString, values);
-    // 判断是否有第三方的连接，否则使用默认连接
-    let p = conn ? Promise.resolve(conn) : this._getConnection();
-    return p.then(conn => {
-      return new Promise((resolve, reject) => {
-        conn.query(sql, params, (err, results, fields) => {
-          // If conn is in transaction, not release
-          // 如果事务结束则释放连接
-          // ？？封装的开启事务，回滚事务该怎么用呢？
-          // 答：请参考单元测试中的事务相关用法
-          if (!conn.inTransaction) {
-            this._releaseConnection(conn);
-          }
-          // 返回查询的值
-          if (err) {
-            return reject(err);
-          }
-          resolve(results);
+  _execute(sqlString, values, client) {
+    const { sql, params } = this._processSqlAndParameter(sqlString, values);
+    const p = client ? Promise.resolve(client) : this._getClient();
+    return p.then(client => {
+      return client
+        .query(sql, params)
+        .then(res => {
+          this._releaseClient(client);
+          return res;
+        })
+        .catch(err => {
+          this._releaseClient(client);
+          return Promise.reject(err);
         });
-      });
     });
+
+    // , (err, results, fields) => {
+    //   // If conn is in transaction, not release
+    //   if (!conn.inTransaction) {
+    //     this._releaseConnection(conn);
+    //   }
+    //   // 返回查询的值
+    //   if (err) {
+    //     return reject(err);
+    //   }
+    //   resolve(results);
+    // });
+    // });
   }
   // 格式化参数
   _processSqlAndParameter(sqlString, params) {
     let result;
     // If is an array, direct return.
-    // 如果参数已经是一个数组，直接返回
     if (Array.isArray(params)) {
       result = {
         sql: sqlString,
@@ -184,17 +138,16 @@ class PostgreSQLClient {
       };
     } else {
       // Replace object to array, and use ? replace @Property
-      let paramArr = [];
+      const paramArr = [];
       if (params) {
-        let paramKeys = Object.keys(params);
+        const paramKeys = Object.keys(params);
+        let index = 0;
         sqlString = sqlString.replace(/@[a-zA-Z0-9_]+/g, (match, offset, str) => {
-          // 首先把匹配的占位符@去掉
-          let matchKey = match.replace('@', '');
-          // 接着从参数对象中通过键名查找是否有对应的值，如果有则返回值，否则返回？
-          // 最后返回将占位符替换为具体的值
+          const matchKey = match.replace('@', '');
           if (paramKeys.indexOf(matchKey) >= 0) {
             paramArr.push(params[matchKey]);
-            return '?';
+            index++;
+            return `$${index}`;
           }
           return match;
         });
