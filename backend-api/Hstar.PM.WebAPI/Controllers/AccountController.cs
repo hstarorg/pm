d26a91838b19e97290cc13de7bdf360547e0de7d
jh3r <jay.m.hu@newegg.com>
2018-06-29 15:38:43 +0800

    Update account controller

---
+++ backend-api/Hstar.PM.WebAPI/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hstar.PM.Business;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hstar.PM.WebAPI.Controllers
+{
+    [Route("account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountBusiness _accountBiz;
+        public AccountController(IAccountBusiness accountBiz)
+        {
+            this._accountBiz = accountBiz;
+        }
+
+        [HttpGet, Route("login")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2", this._accountBiz.GetName() };
+        }
+
+    }
+}
